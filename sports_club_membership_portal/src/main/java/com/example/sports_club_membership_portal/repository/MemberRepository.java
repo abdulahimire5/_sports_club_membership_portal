522877a15651caa0b462bf123669349a3d38452a
Abdulahi Yasin <mymusic19bbc@gmail.com>
2026-07-08 00:42:24 +0300

    Initial commit

--- sports_club_membership_portal/src/main/java/com/example/sports_club_membership_portal/repository/MemberRepository.java
+++ sports_club_membership_portal/src/main/java/com/example/sports_club_membership_portal/repository/MemberRepository.java
@@ -3,7 +3,7 @@ package com.example.sports_club_membership_portal.repository;
 import com.example.sports_club_membership_portal.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
+
 
 public interface MemberRepository extends JpaRepository<Member, Integer> {
     }
