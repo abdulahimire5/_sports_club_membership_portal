05cc21177a17f9a77124413f296198b92b800b34
rukia <rukiahashi282@gmail.com>
2026-07-08 05:10:07 -0700

    intial commit

---
+++ sports_club_membership_portal/src/main/java/com/example/sports_club_membership_portal/controller/TrainingSessionController.java
@@ -0,0 +1,46 @@
+package com.example.sports_club_membership_portal.controller;
+import com.example.sports_club_membership_portal.dto.TrainingSessionRequestDTO;
+import com.example.sports_club_membership_portal.dto.TrainingSessionResponseDTO;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/training-sessions")
+@RequiredArgsConstructor
+public class TrainingSessionController {
+
+        private final TrainingSessionService trainingSessionService;
+
+        @GetMapping
+        public List<TrainingSessionResponseDTO> getAllTrainingSessions() {
+            return trainingSessionService.getAllTrainingSessions();
+        }
+
+        @GetMapping("/{id}")
+        public TrainingSessionResponseDTO getTrainingSessionById(@PathVariable Long id) {
+            return trainingSessionService.getTrainingSessionById(id);
+        }
+
+        @PostMapping
+        public TrainingSessionResponseDTO createTrainingSession(
+                @Valid @RequestBody TrainingSessionRequestDTO requestDTO) {
+            return trainingSessionService.createTrainingSession(requestDTO);
+        }
+
+        @PutMapping("/{id}")
+        public TrainingSessionResponseDTO updateTrainingSession(
+                @PathVariable Long id,
+                @Valid @RequestBody TrainingSessionRequestDTO requestDTO) {
+            return trainingSessionService.updateTrainingSession(id, requestDTO);
+        }
+
+        @DeleteMapping("/{id}")
+        public String deleteTrainingSession(@PathVariable Long id) {
+            trainingSessionService.deleteTrainingSession(id);
+            return "Training Session deleted successfully.";
+        }
+    }
+
