6cb045bb6d26d59de20f0e2e1010aaba7984965a
abdulahimire5 <mymusic19bbc@gmail.com>
2026-07-26 00:35:40 +0300

    Update MemberController.java

--- sports_club_membership_portal/src/main/java/com/example/sports_club_membership_portal/controller/MemberController.java
+++ sports_club_membership_portal/src/main/java/com/example/sports_club_membership_portal/controller/MemberController.java
@@ -18,7 +18,7 @@ public class MemberController {
     public List<Member> getAll() {
         return memberService.getAllMembers();
     }
-    @GetMapping("/{id}")
+    @GetMapping("/{}")
     public Member getOne(@PathVariable Integer id) {
         return memberService.getMemberById(id);
     }
